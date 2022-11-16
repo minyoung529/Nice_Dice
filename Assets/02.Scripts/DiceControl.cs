761201310900c9dc16a216fa76891d6f0b10d6a6
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-11-16 23:54:13 +0900

    스크립트 명 변경

--- Assets/02.Scripts/DiceControl.cs
+++ Assets/02.Scripts/DiceControl.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class DiceRotate : MonoBehaviour
+public class DiceControl : MonoBehaviour
 {
+    public bool IsRotate = false;
     [SerializeField]
     private Vector3 vector = new Vector3(0, 1, 1);
     [SerializeField]
-    private float speed = 5f;
-    [SerializeField]
-    private bool IsRotate = false;
+    private float speed = 100f;
 
     void Update()
     {
