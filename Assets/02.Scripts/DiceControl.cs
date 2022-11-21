db0fc29604388d47949e3e0e891df6a3e2ca0316
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-11-21 23:30:58 +0900

    확률 완성

--- Assets/02.Scripts/DiceControl.cs
+++ Assets/02.Scripts/DiceControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEditor;
+
 public enum DiceShape
 {
     Cube,
@@ -19,11 +20,13 @@ public class DiceControl : MonoBehaviour
     }
 
     [SerializeField]
-    private Vector3 vector = new Vector3(0, 1.5f, 1.5f);
+    private Vector3 rotateVector = new Vector3(0, 1.5f, 1.5f);
     [SerializeField]
-    private float speed = 100f;
+    private float speed = 1f;
     [SerializeField]
     private bool isRotate = false;
+    [SerializeField]
+    private bool isDrop = false;
 
     [SerializeField]
     private DiceShape diceShape = DiceShape.Unknown;
@@ -44,22 +47,56 @@ public class DiceControl : MonoBehaviour
     {
         if (IsRotate)
         {
-            transform.Rotate(vector.x, vector.y, vector.z, Space.Self);
+            transform.Rotate(rotateVector.x, rotateVector.y, rotateVector.z, Space.World);
         }
     }
 
-    public int DiceNumSelect(DiceShape shape, int grade)
+    /// <summary>
+    /// 올릴 주사위 면을 고르는 함수 
+    /// </summary>
+    /// <param name="shape">주사위 모양</param>
+    /// <param name="grade">랜덤 단계값</param>
+    /// <param name="MAX_GRADE">최대 단계값</param>
+    /// <returns></returns>
+    public int DiceSideSelect(DiceShape shape, int grade, int MAX_GRADE = 12)
     {
         int max = diceList[(int)shape].Length;
-        return 0;
+
+        int section = MAX_GRADE / max;
+        grade += section - 1;
+
+        int val = grade / section;
+
+        if (val == max) { val = 0; } // 0~11 이 아닌 1~12를 기준으로 계산하니까 끝값에 걸리면 인덱스에서 넘침, 고로 딱 그부분을 0으로 되돌리면 해결 WA!
+
+        Debug.Assert(!(val >= max) || val > 0, $"val({val}) is out of index, grade({grade}), section({section})");
+
+        if (Random.Range(0, 2) != 0) 
+        {
+            val = Random.Range(0, max);
+        }
+
+        return val;
     }
 
-    [ContextMenu("TestSideUp")]
+    /// <summary>
+    /// 주사위 면을 올리는 함수
+    /// </summary>
+    /// <param name="shape">주사위 모양</param>
+    /// <param name="sideIdx">올릴 주사위 면의 배열 인덱스</param>
     public void DiceSideUp(DiceShape shape = DiceShape.Cube, int sideIdx = 0)
     {
         isRotate = false;
         Vector3[] vectors = diceList[(int)shape];
         Vector3 upSide = vectors[sideIdx];
-        transform.DORotate(upSide, speed * Time.deltaTime, RotateMode.Fast);
+        transform.DORotate(upSide, speed, RotateMode.Fast);
+        Debug.Log($"{sideIdx} side is Up");
+    }
+
+    [ContextMenu("Test")]
+    public void TestFunc() //테스트 용 함수 
+    {
+        int rand = Random.Range(0, 12); Debug.Log(rand);
+        DiceSideUp(diceShape, DiceSideSelect(diceShape, rand));
     }
 }
