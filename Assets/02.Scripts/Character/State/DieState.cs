1d844d0ed26fb44cb972ad8e70e2fd1db8ccc883
minyoung529 <dlalsdud102613@gmail.com>
2022-12-19 12:35:18 +0900

    DieState 더블 호출 고침

--- Assets/02.Scripts/Character/State/DieState.cs
+++ Assets/02.Scripts/Character/State/DieState.cs
@@ -17,7 +17,6 @@ public class DieState : StateBase
     {
         character.Animator.SetTrigger(hashDie);
         timer = 0f;
-        delayCmp = false;
         //isCallOnce = false;
     }
 
@@ -28,17 +27,16 @@ public class DieState : StateBase
         if (timer > 3f)
         {
             timer = 0f;
-            delayCmp = true;
-            OnEnd();
+            character.ChangeState(CharacterState.Idle);
         }
     }
 
     public override void OnEnd()
     {
-        if (!isCallOnce) { return; }
-        if (!delayCmp) return;
+        //if (!isCallOnce) { return; }
+        //if (!delayCmp) return;
 
-        isCallOnce = false;
+        //isCallOnce = false;
 
         if (character.CompareTag("Player")) // ∞‘¿” ≥°
         {
@@ -55,6 +53,6 @@ public class DieState : StateBase
             GameManager.Instance.UI.HeaderUIController.UpdateUI();
         }
 
-        isCallOnce = true;
+        //isCallOnce = true;
     }
 }
